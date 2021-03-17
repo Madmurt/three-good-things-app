76ddd78081a8457f294e2b3a468cb6209509a5f2
Madmurt <mccartm6@gmail.com>
2021-03-17 20:21:37 +0000

    cleanup

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
 
 const devConfig = {
 	apiKey: process.env.REACT_APP_DEV_API_KEY,
@@ -25,6 +26,7 @@ class Firebase {
 	constructor() {
 		app.initializeApp(config);
 		this.auth = app.auth();
+		this.db = app.database();
 	}
 
 	// *** Auth API ***
@@ -41,6 +43,12 @@ class Firebase {
 
 	doPasswordUpdate = (password) =>
 		this.auth.currentUser.updatePassword(password);
+
+	// *** User API ***
+
+	user = (uid) => this.db.ref(`users/${uid}`);
+
+	users = () => this.db.ref('users');
 }
 
 export default Firebase;
